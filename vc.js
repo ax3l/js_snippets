b1e73c1cae5b202c02f1214ed7eb19ca087f799b
Axel Huebl <axel.huebl@plasma.ninja>
2016-06-01 16:46:58 +0200

    Fix External Load

--- vc.js
+++ vc.js
@@ -1,4 +1,8 @@
-window.onload = function() {
+window.addEventListener("DOMContentLoaded", function() {
   document.body.style.backgroundColor="#EEE";
   document.getElementById('col4_content').style.backgroundColor="#EEE";
-}
+
+  // add PIConGPU logo
+  var content = document.getElementById("col4_content");
+  content.innerHTML += "<img alt='PIConGPU' target='_blank' src='https://raw.githubusercontent.com/ComputationalRadiationPhysics/picongpu/dev/doc/logo/pic_logo_320x140.png' />";
+}, false);
