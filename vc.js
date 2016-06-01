98bf29100fe009461d64d34a997b4a97ff324c88
Axel Huebl <axel.huebl@plasma.ninja>
2016-06-01 17:24:21 +0200

    Vcard

--- vc.js
+++ vc.js
@@ -1,8 +1,9 @@
 window.addEventListener("DOMContentLoaded", function() {
   document.body.style.backgroundColor="#EEE";
+  document.getElementById('col5').style.backgroundColor="#EEE";
   document.getElementById('col4_content').style.backgroundColor="#EEE";
 
   // add PIConGPU logo
   var content = document.getElementById("col4_content");
-  content.innerHTML += "<img alt='PIConGPU' target='_blank' src='https://raw.githubusercontent.com/ComputationalRadiationPhysics/picongpu/dev/doc/logo/pic_logo_320x140.png' />";
+  content.innerHTML += "<img alt='PIConGPU' target='_blank' src='https://rawgit.com/ax3l/js_snippets/master/vcard.png' />";
 }, false);
